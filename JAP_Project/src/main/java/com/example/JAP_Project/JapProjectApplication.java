842cd3e54c020c77bab7ab3f8000a9d1786fbbcf
양해정 <127839004+Haejeong079@users.noreply.github.com>
2023-09-11 20:47:41 +0900

    Second commit

---
+++ JAP_Project/src/main/java/com/example/JAP_Project/JapProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.JAP_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapProjectApplication.class, args);
+	}
+
+}
---
+++ JAP_Project/src/test/java/com/example/JAP_Project/JapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JAP_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
